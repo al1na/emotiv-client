739e10c5508542e4848b52a45353e2ea99a0599b
al1na <al1na@users.noreply.github.com>
2015-04-28 16:58:01 +0200

    cleanup timeit

--- EmotivClient-timeit.py
+++ EmotivClient-timeit.py
@@ -1,10 +1,9 @@
 """
 Usage:
-  EmotivClient-timeit.py demo
   EmotivClient-timeit.py save_to_sqldb
   EmotivClient-timeit.py save_to_csv
   EmotivClient-timeit.py save_to_jsonfile
-  EmotivClient-timeit.py save [--nrexperiments=<nr>] (tocsv | tosqldb | tojsonfile) [compress (gzip | 7zip)]
+  EmotivClient-timeit.py save [--nrexperiments=<nr>] (tocsv | tosqldb | tojsonfile) [compress (gzip | 7zip | bzip)]
 
 Options:
   --nrexperiments=<nr>
@@ -168,6 +167,7 @@ def main(arguments):
     for label, xi, yi in zip(labels, x, y):
         plt.annotate(label, xy = (xi, yi), xytext = (-10, 10), textcoords = 'offset points')
     plt.show()
+    plt.savefig("justsaving.png", dpi=150)
 
     ''' Plot 2: Memory-time plot for archived files '''
 
@@ -189,15 +189,44 @@ def main(arguments):
     s7 = size_sqlite_bzip_file
     s8 = size_sqlite_7zip_file
     s9 = size_sqlite_gzip_file
+    #x = [t1, t2, t3, t4, t5, t6, t7, t8, t9]
+    #y = [s1, s2, s3, s4, s5, s6, s7, s8, s9]
+    #labels = ['csv/bzip', 'csv/7zip', 'csv/gzip', 'json/bzip', 'json/7zip', 'json/gzip', 'sqlite/bzip', 'sqlite/7zip', 'sqlite/gzip']
+    #plt.xlabel('Time in seconds')
+    #plt.ylabel('Memory in bytes')
+    #plt.scatter(x, y, s=20)
+    #for label, xi, yi in zip(labels, x, y):
+    #    plt.annotate(label, xy = (xi, yi), xytext = (-10, 10), textcoords = 'offset points')
+    #plt.show()
+
+    lg = []
+    #x = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     x = [t1, t2, t3, t4, t5, t6, t7, t8, t9]
+    #y = [11, 25, 13, 34, 51, 44, 19, 39, 6]
     y = [s1, s2, s3, s4, s5, s6, s7, s8, s9]
+    colors = ['b', 'g', 'r', 'b', 'g', 'r', 'b', 'g', 'r']
+    markers = ['s', 's', 's', 'o', 'o', 'o', 'v', 'v', 'v']
     labels = ['csv/bzip', 'csv/7zip', 'csv/gzip', 'json/bzip', 'json/7zip', 'json/gzip', 'sqlite/bzip', 'sqlite/7zip', 'sqlite/gzip']
+
     plt.xlabel('Time in seconds')
     plt.ylabel('Memory in bytes')
-    plt.scatter(x, y, s=20)
+
+    for i in range(0, 9):
+        lg.append(plt.scatter(x[i], y[i], marker=markers[i], color=colors[i], s=100))
+
     for label, xi, yi in zip(labels, x, y):
         plt.annotate(label, xy = (xi, yi), xytext = (-10, 10), textcoords = 'offset points')
-    plt.show()
+
+    plt.legend((lg[0], lg[1], lg[2], lg[3], lg[4], lg[5], lg[6], lg[7], lg[8]),
+               ('csv/bzip', 'csv/7zip', 'csv/gzip', 'json/bzip', 'json/7zip', 'json/gzip', 'sqlite/bzip', 'sqlite/7zip', 'sqlite/gzip'),
+               scatterpoints=1,
+               loc='upper right',
+               ncol=3,
+               fontsize=10,
+               bbox_to_anchor=(0., 1.02, 1., .102),
+               mode="expand",
+               borderaxespad=0.)
+    plt.savefig("saving+compressing.png", dpi=150)
 
     if arguments['save_to_csv']:
         save_to_csv_timeit(nrexp)
@@ -205,20 +234,6 @@ def main(arguments):
         save_to_sqldb_timeit(nrexp)
     elif arguments['save_to_jsonfile']:
         save_to_jsonfile_timeit(nrexp)
-    elif arguments['save']:
-        print "Save"
-        if arguments['tocsv']:
-            print "tocsv"
-        elif arguments['tosqldb']:
-            print "tosqldb"
-
-        if (arguments['compress']):
-            print "compress"
-            if (arguments['7zip']):
-                print "7zip"
-    else:
-        print "Invalid argument."
-
 
 if __name__ == "__main__":
     arguments = docopt(__doc__)
